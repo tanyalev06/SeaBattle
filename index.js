584b8e752661a0fc90ef2725b6a6e324cbe04e28
tanyalev06 <grib.tanya06@mail.ru>
2022-01-10 21:28:26 +0300

    create logic game

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const infoDisplay = document.querySelector('#info')
   const userSquares = [];
   const computerSquares = [];
+  let isHorizontal = true;
+  let isGameOver = false;
+  let currentPlayer = 'user';
 
   const width = 10;
-
+  
   function createBoard(grid, squares, width) {
     for (let i=0; i<width*width; i++){
       const square = document.createElement('div');
@@ -87,6 +90,136 @@ document.addEventListener('DOMContentLoaded', () => {
   generate(shipArray[2]);
   generate(shipArray[3]);
   generate(shipArray[4]);
+
+  function rotate() {
+    if(isHorizontal){
+      destroyer.classList.toggle('destroyer-container-vertical');
+      submarine.classList.toggle('submarine-container-vertical');
+      cruiser.classList.toggle('cruiser-container-vertical');
+      buttleship.classList.toggle('buttleship-container-vertical');
+      carrier.classList.toggle('carrier-container-vertical');
+      isHorizontal = false;
+      console.log(isHorizontal)
+      return;
+    }
+    if(!isHorizontal){
+      destroyer.classList.toggle('destroyer-container-vertical');
+      submarine.classList.toggle('submarine-container-vertical');
+      cruiser.classList.toggle('cruiser-container-vertical');
+      buttleship.classList.toggle('buttleship-container-vertical');
+      carrier.classList.toggle('carrier-container-vertical');
+      isHorizontal = true;
+      console.log(isHorizontal)
+      return;
+  }
+}
+  rotateButton.addEventListener('click', rotate);
+
+  ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
+  userSquares.forEach(square => square.addEventListener('dragstart', dragStart))
+  userSquares.forEach(square => square.addEventListener('dragover', dragOver))
+  userSquares.forEach(square => square.addEventListener('dragenter', dragEnter))
+  userSquares.forEach(square => square.addEventListener('dragleave', dragLeave))
+  userSquares.forEach(square => square.addEventListener('drop', dragDrop))
+  userSquares.forEach(square => square.addEventListener('dragend', dragEnd))
+
+  let selectedShipNameWithIndex
+  let draggedShip
+  let draggedShipLength
+
+
+  ships.forEach(ship => ship.addEventListener('mousedown',(e) => {
+    selectedShipNameWithIndex = e.target.id;
+    console.log(selectedShipNameWithIndex)
+  }))
+
+  function dragStart(){
+    draggedShip = this
+    draggedShipLength = this.childNodes.length
+    
+  console.log(draggedShip)
+  }
+
+  function dragOver(e){
+    e.preventDefault()
+  }
+
+  function dragEnter(e){
+    e.preventDefault()
+  }
+
+  function dragLeave(){
+    console.log('drag leave')
+  }
+
+  function dragDrop(){
+   let shipNameWithLastId = draggedShip.lastChild.id
+   let shipClass = shipNameWithLastId.slice(0, -2)
+   console.log(shipClass)
+   let lastShipIndex = parseInt(shipNameWithLastId.substr(-1))
+   let shipLastId = lastShipIndex + parseInt(this.dataset.id)
+   console.log(shipLastId)
+   const notAllowedHorizontal = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 1, 11, 21, 31, 41, 51, 61, 71, 81, 91, 2, 12, 22, 32, 42, 52, 62, 72, 82, 92,
+                                  3, 13, 23, 33, 43, 53, 63, 73, 83, 93]
+   const notAllowedVertical = [99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71,
+                               70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60]
+  let newnotAllowedHorizontal = notAllowedHorizontal.splice(0, 10 * lastShipIndex)
+  let newnotAllowedVertical = notAllowedVertical.splice(0, 10 * lastShipIndex)
+
+   selectedShipIndex = parseInt(selectedShipNameWithIndex.substr(-1))
+
+   shipLastId = shipLastId - selectedShipIndex
+   console.log(shipLastId)
+
+   if(isHorizontal && !newnotAllowedHorizontal.includes(shipLastId)){
+     for (let i = 0; i< draggedShipLength; i++){
+       userSquares[parseInt(this.dataset.id) - selectedShipIndex + i].classList.add('taken', shipClass)
+     }
+   }else if(!isHorizontal && !newnotAllowedVertical.includes(shipLastId)){
+    for (let i = 0; i< draggedShipLength; i++){
+     userSquares[parseInt(this.dataset.id) - selectedShipIndex + width*i].classList.add('taken', shipClass)
+   }
+ }else return
+
+ displayGrid.removeChild(draggedShip)
+}
+
+
+  function dragEnd(){
+    console.log('dragged')
+  }
+
+  function playGame(){
+    if(isGameOver) return
+    if(currentPlayer === 'user'){
+      turnDisplay
+      computerSquares.forEach(square => square.addEventListener('click', function(e) {
+        revealSquare(square)
+      }))
+    }
+    if(currentPlayer === 'computer'){
+      turnDisplay.innerHTML = 'Computers go'
+    }
+  }
+  startButton.addEventListener('click', playGame)
+
+  let destroyerCount = 0;
+  let submarineCount = 0;
+  let cruiserCount = 0;
+  let buttleShipCount = 0;
+  let carrierCount = 0;
+  function revealSquare(square) {
+    if (square.classList.contains('destroyer')) destroyerCount++
+    if (square.classList.contains('submarine')) submarineCount++
+    if (square.classList.contains('cruiser')) cruiserCount++
+    if (square.classList.contains('buttleShip')) buttleShipCount++
+    if (square.classList.contains('carrierShip')) carrierCount++
+
+    if(square.classList.contains('taken')) {
+      square.classList.add('boom');
+      console.log('click');
+    }
+  }
 
 })
 
