e3a2bb5da2a8287bc91b607912f929f9d72f306d
tanyalev06 <grib.tanya06@mail.ru>
2022-01-10 11:25:49 +0300

    create generate function

--- index.js
+++ index.js
@@ -1,4 +1,98 @@
-var location1 = 3;
+document.addEventListener('DOMContentLoaded', () => {
+  const userGrid = document.querySelector('.grid-user');
+  const computerGrid = document.querySelector('.grid-computer');
+  const displayGrid = document.querySelector('.grid-display');
+  const ships = document.querySelectorAll('.ship');
+  const destroyer = document.querySelector('.destroyer-container');
+  const submarine = document.querySelector('.submarine-container');
+  const cruiser = document.querySelector('.cruiser-container');
+  const buttleship = document.querySelector('.buttleship-container');
+  const carrier = document.querySelector('.carrier-container');
+  const startButton =document.querySelector('#start');
+  const rotateButton = document.querySelector('#rotate');
+  const turnDisplay = document.querySelector('#whose-go');
+  const infoDisplay = document.querySelector('#info')
+  const userSquares = [];
+  const computerSquares = [];
+
+  const width = 10;
+
+  function createBoard(grid, squares, width) {
+    for (let i=0; i<width*width; i++){
+      const square = document.createElement('div');
+      square.dataset.id = i;
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+
+  createBoard(userGrid, userSquares, width);
+  createBoard(computerGrid, computerSquares, width);
+
+  const shipArray = [
+    {
+    name: 'destroyer',
+    directions: [
+    [0, 1],
+    [0, width]
+    ]
+    },
+    {
+      name: 'submarine',
+      directions: [
+        [0, 1, 2],
+        [0, width, width*2]
+      ]
+    },
+    {
+      name: 'cruiser',
+      directions: [
+        [0, 1, 2],
+        [0, width, width*2]
+      ]
+    },
+    {
+      name: 'buttleship',
+      directions: [
+        [0, 1, 2, 3],
+        [0, width, width*2, width*3]
+      ]
+    },
+    {
+      name: 'carrier',
+      directions: [
+        [0, 1, 2, 3, 4],
+        [0, width, width*2, width*3, width*4]
+      ]
+    },
+]
+
+  function generate(ship) {
+    let randomDirection = Math.floor(Math.random() * ship.directions.length);
+    let current = ship.directions[randomDirection];
+    if (randomDirection === 0) direction = 1;
+    if (randomDirection === 1) direction = 10;
+    let randomStart = Math.abs(Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction)));
+
+    const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'));
+    const isAtRightEdge = current.some(index => (randomStart + index) % width === width -1);
+    const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0);
+
+    if(!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart + index].classList.add('taken', ship.name));
+    else generate(ship);
+  }
+
+  generate(shipArray[0]);
+  generate(shipArray[1]);
+  generate(shipArray[2]);
+  generate(shipArray[3]);
+  generate(shipArray[4]);
+
+})
+
+
+
+/*var location1 = 3;
 var location2 = 4;
 var location3 = 5;
 
@@ -7,21 +101,21 @@ var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while(isSunk == false){
-    guess = prompt("Hit it! Enter number from 0 to 6!")
-    if(guess < 0 || guess > 6){
-        alert('You have entered wrong number: enter number from 0 to 6 only');
-    }else{
-        guesses = guesses + 1;
-            if(guess == location1 || guess == location2 || guess == location3){
-                hits = hits + 1;
-                if(hits == 3){
-                    isSunk = true;
-                    alert('Congrats!You are win!');
-            }
-        }
-            else{
-                alert('You are lost')
-        }
+while (isSunk == false) {
+  guess = prompt("Hit it! Enter number from 0 to 6!");
+  if (guess < 0 || guess > 6) {
+    alert("You have entered wrong number: enter number from 0 to 6 only");
+  } else {
+    guesses = guesses + 1;
+    if (guess == location1 || guess == location2 || guess == location3) {
+      hits = hits + 1;
+      if (hits == 3) {
+        isSunk = true;
+        alert("Congrats!You are win!");
+      }
+    } else {
+      alert("You are lost");
     }
+  }
 }
+*/
